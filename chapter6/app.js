fa005dd66348a82c7167fdece3a532f7c2ec0172
DivadTenni <davidctenni@gmail.com>
2022-02-13 18:12:39 +1100

    added: some parent and child relationship stuff

--- chapter6/app.js
+++ chapter6/app.js
@@ -25,7 +25,7 @@ heading.innerText = 'If you aim for the king make sure you do nto miss';
 
 //selecting element by div with class
 const secondText = document.querySelector('p.secondryText');
-console.log(secondText);
+//console.log(secondText);
 secondText.innerText = 'some more Text';
 
 
@@ -39,22 +39,22 @@ secondText.innerText = 'some more Text';
 
 //.grab element by id
 const title =document.getElementById('title')
-console.log(title);
+//console.log(title);
 
 //get element by class name
 //this returns a html collection so you cannot for all on it or other common methdos that objects can have
 const elementBasedOnClassName = document.getElementsByClassName('paraTexts');
-console.log(elementBasedOnClassName);
+//console.log(elementBasedOnClassName);
 
 
 //get element by tag name
 const documentByTagName =document.getElementsByTagName('p');
 //grabbing all the ps again
-console.log(documentByTagName);
+//console.log(documentByTagName);
 
 
 const getFromTheDom = document.getElementById('manMe');
-console.log(getFromTheDom)
+//console.log(getFromTheDom)
 getFromTheDom.innerText = 'hanging this stuff';
 getFromTheDom.innerHTML = '<h3> grow big and stronk </h3>';
 
@@ -82,19 +82,19 @@ lads.forEach(lad =>
 //Updating attributes
 
 const link = document.getElementById('fix');
-console.log(link);
+//console.log(link);
 link.getAttribute('href')
 link.setAttribute('href','https://www.bing.com')
 const link2 = document.getElementById('fix2');
 
 //could use this kind of thing for debugging or soemthing
 link2.innerText = link2.getAttribute('class');
-console.log(link2.getAttribute);
+//console.log(link2.getAttribute);
 
 
 //changing css styles say setting colour
 let stylishHeading = document.getElementById('stylish')
-console.log(stylishHeading); 
+//console.log(stylishHeading); 
 stylishHeading.style.color = 'crimson';
 stylishHeading.style.fontSize = '42';
 stylishHeading.style.accentColor = 'orange';
@@ -107,7 +107,7 @@ stylishHeading.classList.remove('veryCoolClass');
 //getting all the things and changing class based on innertext
 
 let allThePs = document.querySelectorAll('p');
-console.log(allThePs);
+//console.log(allThePs);
 //convert html collection to array
 //allThePs = Array.prototype.slice.call(allThePs);
 allThePs.forEach( p =>
@@ -126,4 +126,36 @@ allThePs.forEach( p =>
  const titler = document.querySelector('.titular');
  titler.classList.toggle('test');
 
- main();
+
+ //relationships between different elements in the dom
+ //sibling relationship ones on same level from same aprent are siblings 
+ //parent child is what it always is
+ 
+//grab html collection of children
+ const article = document.querySelector('article');
+ //console.log(article.children);
+
+//convert html collection to array
+//this is not destructive and does nto alter original value
+
+Array.from(article.children).forEach(childElement => {
+    console.log('s');
+childElement.classList.add('article-element');
+})
+
+
+//console.log(article);
+
+//getting the parent of an element
+const title3 =document.querySelector('h4');
+console.log(title3);
+
+console.log(title3.parentElement);
+//chains work
+console.log(title3.parentElement.parentElement);
+
+
+
+
+
+main();
