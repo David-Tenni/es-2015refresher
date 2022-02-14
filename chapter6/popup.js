49190afbb306171c4a5685f0e6e4eac5f510ddb2
DivadTenni <davidctenni@gmail.com>
2022-02-14 17:46:53 +1100

    fixed: popup null ref

--- chapter6/popup.js
+++ chapter6/popup.js
@@ -1,9 +1,22 @@
 const button = document.querySelector('button');
 const popup = document.querySelector('.popup-wrapper');
+
 const closePopup = document.querySelector('.popup-close');
 
+button.addEventListener('click', () => {
 
+    popup.style.display = 'block';
 
-button.addEventListener('click', () => { popup.style.display ='block';});
-closePopup.addEventListener('click', () => { popup.style.display ='none';});
-popup.addEventListener('click', () => { popup.style.display ='none';});
+});
+
+closePopup.addEventListener('click', () => {
+
+    popup.style.display = 'none';
+
+});
+
+popup.addEventListener('click', () => {
+
+    popup.style.display = 'none';
+
+});
