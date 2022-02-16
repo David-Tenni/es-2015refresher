72a5448b479e752aceff6e5e936f3f67bba7efee
DivadTenni <davidctenni@gmail.com>
2022-02-16 22:24:28 +1100

    added: delete functionality to lit items

--- chapter10/app.js
+++ chapter10/app.js
@@ -3,18 +3,22 @@ const taskList = document.getElementById('taskList');
 
 console.log(addForm);
 
+//add the task
 const AddTask = todo => {
           
     const newItem = 
-    `<li class="list-group-item d-flex justify-content-between align-items-center">
-    <span> ${todo} <span>
-    <i class="far fa-trash-alt delete"></i>          
+    `
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+            <span>${todo}</span>
+            <i class="far fa-trash-alt delete"></i>
+          </li>
     `;
     taskList.innerHTML += newItem;
     
 };
 
 
+//event listener which checks to add new task
 addForm.addEventListener('submit', e => {
     e.preventDefault();
     const task = addForm.add.value.trim();
@@ -25,4 +29,10 @@ addForm.addEventListener('submit', e => {
     };
 });
 
+//delete a task
+taskList.addEventListener('click', e => {
+    if(e.target.classList.contains('delete')){
+        e.target.parentElement.remove();
+    }
+});
 
