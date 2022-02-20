1df2f15093cd60139e44fa421b05babd24f42a0a
DivadTenni <davidctenni@gmail.com>
2022-02-20 15:33:01 +1100

    added a promise to the request function

--- chapter12/app.js
+++ chapter12/app.js
@@ -25,36 +25,50 @@ countToFive();
 
 
 const requestFunction = (resource, callback) => {
-
-    const request = new XMLHttpRequest();
-  
-    request.addEventListener('readystatechange', () => {
-        //ensuring that there is a ready state of 4 and status of 200 which means everything is good
-      if(request.readyState === 4 && request.status === 200){
-        const data = JSON.parse(request.responseText);
-        callback(undefined, data);
-      } else if (request.readyState === 4){
-        callback('could not fetch the data', undefined);
-      }
-  
+    return new Promise((resolve, reject) => {
+        const request = new XMLHttpRequest();
+      
+        request.addEventListener('readystatechange', () => {
+            //ensuring that there is a ready state of 4 and status of 200 which means everything is good
+          if(request.readyState === 4 && request.status === 200){
+            const data = JSON.parse(request.responseText);
+            resolve(data);
+          } else if (request.readyState === 4){
+            reject('error calling api');
+          }
+      
+        });
+        
+        request.open('GET', resource);
+        request.send();
     });
-    
-    request.open('GET', resource);
-    request.send();
   
 };
   
   
-requestFunction('todo/bob.json', (err, data) => {
-    console.log(data);
-    requestFunction('todo/jason.json', (err, data) => {
-      console.log(data);
-      requestFunction('todo/sam.json', (err, data) => {
-        console.log(data);
-      });
-    });
+requestFunction('todo/bob.json').then( data =>{
+console.log('promise resolved', data);
+}).then((error) => {
+console.log(error);
 });
 
 
-//promises
+//promises inside a promise you either resolve it when you scuesfful get the data or reject it when you get some kind of error
+
+const promiseGetSomething = () => {
+    return new Promise((resolve, reject) => {
+        //resolve('data');
+        //reject('error');
+    });
+};
+//promiseGetSomething().then((data) => {
+//    console.log(data)
+//});
+
+//refactoring of the above
+promiseGetSomething()
+.then(data => {console.log(data)})
+.catch(error => {
+    console.log(error);
+});
 
