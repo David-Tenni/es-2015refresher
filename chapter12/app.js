15bb3aeb3a75ac232caa30a498914feb1469d04c
DivadTenni <davidctenni@gmail.com>
2022-02-20 11:15:09 +1100

    added: http request

--- chapter12/app.js
+++ chapter12/app.js
@@ -1,15 +1,29 @@
 
-function countSeconds(waitTime){
+const countSeconds  = waitTime =>{
     let second = waitTime;
     setTimeout ((waitTime) => {
-        console.log(`${second} seconds since start counting` );
+        console.log(`${second} seconds since started counting` );
     }, waitTime*1000); 
     
 };
 
 
-countSeconds(1);
-countSeconds(2);
-countSeconds(3);
-countSeconds(4);
 
+const countToFive = () => {
+    countSeconds(1);
+    countSeconds(2);
+    countSeconds(3);
+    countSeconds(4);
+    countSeconds(5);
+}
+countToFive();
+
+//request object
+const request = new XMLHttpRequest();
+request.addEventListener('readystatechange', () => {
+    if(request.readyState == 4){
+        console.log(request.responseText);
+    };
+});
+request.open('GET', 'https://jsonplaceholder.typicode.com/todos');
+request.send();
