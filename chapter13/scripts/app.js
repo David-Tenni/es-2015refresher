466bb8d07cdd3d735771939ba0e7aff2c7e4dcdb
DivadTenni <davidctenni@gmail.com>
2022-02-24 00:03:13 +1100

    fixed: issue getting display elements

--- chapter13/scripts/app.js
+++ chapter13/scripts/app.js
@@ -1,16 +1,18 @@
 // let key = document.getElementById('key').innerText;
 // console.log(key);
-const weatherForm = document.getElementById('weather-form');
+const weatherForm = document.querySelector('form');
 const cityDisplay = document.getElementById('current-city')
 const weatherDisplay = document.getElementById('weather-condition')
 const temperatureDisplay = document.getElementById('temperature')
 
 
+const updateCity = async userCity => {
+console.log(userCity)
+};
+
 weatherForm.addEventListener('submit', e => {
 e.preventDefault();
 const city = weatherform.city.value.trim();
-console.log(city);
-debugger;
-cityDisplay.innerText = city;
-weatherForm.requestFullscreen();
+weatherForm.reset();
+updateCity(city);
 });
