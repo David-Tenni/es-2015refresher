0c067de9a6a15cdc15d366de2f77c122e43250e3
DivadTenni <davidctenni@gmail.com>
2022-02-23 17:07:39 +1100

    reverted: changing back to the intended method will change back to using env just for key later

--- chapter13/scripts/forecast.js
+++ chapter13/scripts/forecast.js
@@ -1,3 +1,4 @@
+/*
 let key ='';
 let query='';
 //console.log(key);
@@ -24,6 +25,61 @@ const getCity = async () => {
     
     const response = await fetch(query, {mode: "cors"});
 };
+*/
+
+//Ok so look i'll get the above working but its not critical right now until i get it working ill use a file that's not getting commited "the right way"
 
 
 
+
+// Here is the html 
+
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Weather</title>
+//     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+//     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+//     <link rel="stylesheet" href="styles.css">
+// </head>
+// <body>
+//     <div>
+//     </div>
+//     <div class="container my-5 mx-auto">
+        
+//         <h1 class="text-muted text-center my-4" id="main-title"> Climate Change Potential Weather </h1>
+//         <form class="change-location my-4 text-center text-muted">
+//             <label for="city"> Enter location for future weather predictions...</label>
+//             <input type="text" name="city" class="form-control p-4"></input>
+//         </form>
+//         <div class="card shadow-lg rounded">
+//          <img class="time card-img-top" src="https://via.placeholder.com/400x300" alt="image">
+//             <div class="icon bg-light mx-auto text-center">
+
+
+//             </div>
+//         <div class="text-muted text-uppercase text-center details">
+//             <h5 class="my-3">City</h5>
+//             <div class="my-3">Weather Condition</div>
+//             <div class="display-4 my-4">
+//                 <span>temp</span>
+//                 <span>&deg;C</span>
+//             </div>
+//         </div>
+//         </div>
+//     </div>
+//     <div id="key" class='d-none'> </div>
+//     <div id="query" class='d-none'> </div>
+//     <script type="module">
+//         import { name, value } from './scripts/env/env.js';
+//         let textbox = document.getElementById('key').innerText = value;
+//         //console.log(textbox);
+//     </script>
+//     <script src="scripts/forecast.js"/>
+//     <script src="scripts/env/env.js"/>
+//     <script src="scripts/app.js"/>
+// </body>
+// </html>
