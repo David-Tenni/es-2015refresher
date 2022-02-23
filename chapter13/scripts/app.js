aecf3628862409322b0bacd13d8256346c393e84
DivadTenni <davidctenni@gmail.com>
2022-02-23 23:56:38 +1100

    added: event listener to weather form

--- chapter13/scripts/app.js
+++ chapter13/scripts/app.js
@@ -1,3 +1,16 @@
 // let key = document.getElementById('key').innerText;
 // console.log(key);
+const weatherForm = document.getElementById('weather-form');
+const cityDisplay = document.getElementById('current-city')
+const weatherDisplay = document.getElementById('weather-condition')
+const temperatureDisplay = document.getElementById('temperature')
 
+
+weatherForm.addEventListener('submit', e => {
+e.preventDefault();
+const city = weatherform.city.value.trim();
+console.log(city);
+debugger;
+cityDisplay.innerText = city;
+weatherForm.requestFullscreen();
+});
