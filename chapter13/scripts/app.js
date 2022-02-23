3b5e5bd1364597e946e913fcd6cdf54c66c6e833
DivadTenni <davidctenni@gmail.com>
2022-02-24 00:08:14 +1100

    updated: html form

--- chapter13/scripts/app.js
+++ chapter13/scripts/app.js
@@ -1,6 +1,6 @@
 // let key = document.getElementById('key').innerText;
 // console.log(key);
-const weatherForm = document.querySelector('form');
+const cityForm = document.querySelector('form');
 const cityDisplay = document.getElementById('current-city')
 const weatherDisplay = document.getElementById('weather-condition')
 const temperatureDisplay = document.getElementById('temperature')
@@ -10,9 +10,12 @@ const updateCity = async userCity => {
 console.log(userCity)
 };
 
-weatherForm.addEventListener('submit', e => {
-e.preventDefault();
-const city = weatherform.city.value.trim();
-weatherForm.reset();
-updateCity(city);
-});
+cityForm.addEventListener('submit', e => {
+    // prevent default action
+    e.preventDefault();
+    
+    // get city value
+    const city = cityForm.city.value.trim();
+    cityForm.reset();
+  
+  });
