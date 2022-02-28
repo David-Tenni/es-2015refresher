fdc63ea86709bc56f6119a26513e4e8cff0ce385
DivadTenni <davidctenni@gmail.com>
2022-02-28 17:45:21 +1100

    enhanced: destructured data and refactored citydets to cityDeatils

--- chapter13/scripts/app.js
+++ chapter13/scripts/app.js
@@ -4,13 +4,12 @@ const cityForm = document.querySelector('form');
 const detailsCard = document.getElementById('details-card')
 const details = document.getElementById('details')
 const updateUI = data =>{
-    const cityDets = data.cityDets;
-    const weather = data.weather;
-    console.log(cityDets);
+    const {cityDetails, weather} = data;
+    
     detailsCard.classList.remove('d-none');
     details.innerHTML = 
     `
-    <h5 id='current-city' class="my-3">${cityDets.EnglishName}</h5>
+    <h5 id='current-city' class="my-3">${cityDetails.EnglishName}</h5>
         <div id='weather-condition' class="my-3">${weather.WeatherText}</div>
         <div class="display-4 my-4">
             <span id="temperature" >${weather.Temperature.Metric.Value}</span>
@@ -22,10 +21,10 @@ const updateUI = data =>{
 
 
 const updateCity = async (userCity) => {
-    const cityDets = await getCity(userCity);
-    const weather = await getWeather(cityDets.Key);
-    console.log(cityDets)
-    return { cityDets, weather };
+    const cityDetails = await getCity(userCity);
+    const weather = await getWeather(cityDetails.Key);
+    console.log(cityDetails)
+    return { cityDetails, weather };
 };
 
 
