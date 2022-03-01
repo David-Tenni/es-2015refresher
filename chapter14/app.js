19da6e74cabed8f7f66a6295028f5ad807003db0
DivadTenni <davidctenni@gmail.com>
2022-03-01 17:18:16 +1100

    added: stringifying items array and saving in local storage

--- chapter14/app.js
+++ chapter14/app.js
@@ -1,29 +1,44 @@
 //storing
-localStorage.setItem('Name', 'Bob');
-localStorage.setItem('Age', 80);
-//retrieving 
-let Name = localStorage.getItem('Name');
-console.log(Name);
+OldStuff = () => {
 
-//over writing local storage
+        localStorage.setItem('Name', 'Bob');
+        localStorage.setItem('Age', 80);
+        //retrieving 
+        let Name = localStorage.getItem('Name');
+        console.log(Name);
+        
+        //over writing local storage
+        
+        localStorage.setItem('Name', 'Jason');
+        
+        Name = localStorage.getItem('Name');
+        console.log(Name);
+        
+        
+        //using dot notation
+        localStorage.Name = 'Fred';
+        console.log(localStorage.Name);
+        console.log(localStorage.Age);
+        
+        
+        //deleting data in local storage
+        localStorage.removeItem('Name')
+        console.log(localStorage.Name);
+        
+        
+        //clear all local data
+        localStorage.clear();
+        console.log(localStorage.Age);
+}
 
-localStorage.setItem('Name', 'Jason');
+//Stringifying data
 
-Name = localStorage.getItem('Name');
-console.log(Name);
+const items = [
+        {text: 'something', Name: 'Jeff'},
+        {text: 'something', Name: 'Jeff'},
+        {text: 'something', Name: 'Jeff'},
+]        
 
+localStorage.setItem('items', JSON.stringify(items));
 
-//using dot notation
-localStorage.Name = 'Fred';
-console.log(localStorage.Name);
-console.log(localStorage.Age);
-
-
-//deleting data in local storage
-localStorage.removeItem('Name')
-console.log(localStorage.Name);
-
-
-//clear all local data
-localStorage.clear();
-console.log(localStorage.Age);
+console.log(localStorage.items);
