7b8ef6970dbfe725c4ca707bf7a0b9e7c41aaa76
David Tenni <davidctenni@gmail.com>
2022-03-07 17:57:05 +1100

    added: test user object

--- chapter15/app.js
+++ chapter15/app.js
@@ -1,6 +1,23 @@
 const main = () => {
-
+console.log('hi')
 }
 
 
 main();
+
+
+
+const firstUser = {
+    username: 'jason',
+    email: 'jason@gmail.com',
+    login(){
+        console.log('login')
+    },
+    logout() {
+
+    },
+    
+
+}
+console.log(firstUser.email,firstUser.username)
+firstUser.login();
