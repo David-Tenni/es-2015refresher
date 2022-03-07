e5a53094476685b6b6857cd6828ba5309d1a3e8f
David Tenni <davidctenni@gmail.com>
2022-03-07 18:55:05 +1100

    added: new class user

--- chapter15/app.js
+++ chapter15/app.js
@@ -1,31 +1,52 @@
 const main = () => {
 console.log('hi')
+userFunction();
 }
 
 
-main();
 
 
 
 
 const test = () => {
-
+    
     
     const firstUser = {
-    username: 'jason',
-    email: 'jason@gmail.com',
-    login(){
-        console.log('login')
-    },
-    logout() {
+        username: 'jason',
+        email: 'jason@gmail.com',
+        login(){
+            console.log('login')
+        },
+        logout() {
+            
+        },
         
-    },
-    
-    
-}
-console.log(firstUser.email,firstUser.username)
-firstUser.login();
+        
+    }
+    console.log(firstUser.email,firstUser.username)
+    firstUser.login();
 }
 
 
 
+//classes and proptype models
+
+
+//classes worj by creating a new empty object then binding the value of this to it then caling the constructor ro build it as specified
+
+userFunction = () => {
+    const newUser = new User('jeff', "jeff@jmail.com");
+    console.log(newUser);
+    
+
+
+    
+}
+
+class User{
+    constructor(name, email){
+        this.username = name;
+        this.email = email;
+    }
+}
+main();
